2f365178b0f7b902fefa739802210e7f3bc106ce
Chris Miller <chris_miller@LT-IO-C02X7CL0.local>
2020-12-18 12:41:37 -0600

    Day 18 - refactor to use shunting yard

--- lib/day_18.ex
+++ lib/day_18.ex
@@ -5,65 +5,73 @@ defmodule Day18 do
     """
   end
 
-  def solve_part_1(input) do
+  def solve_part_1(input), do: do_solve(input, %{"+" => 1, "*" => 1})
+  def solve_part_2(input), do: do_solve(input, %{"+" => 2, "*" => 1})
+
+  def do_solve(input, operator_precedences) do
     input
     |> parse()
-    |> Enum.map(&evaluate/1)
+    |> Enum.map(&shunting_yard(&1, operator_precedences))
+    |> Enum.map(&evaluate_reverse_polish/1)
     |> Enum.sum()
   end
 
-  def evaluate(token) when is_list(token), do: Enum.reduce(token, nil, &do_eval/2)
+  def evaluate_reverse_polish(rp_notation, stack \\ [])
+  def evaluate_reverse_polish([value], _stack) when is_integer(value), do: value
 
-  def do_eval(token, nil) when is_integer(token), do: token
-  def do_eval(token, acc) when is_integer(token) and is_function(acc), do: acc.(token)
-  def do_eval("+", acc), do: &Kernel.+(acc, &1)
-  def do_eval("*", acc), do: &Kernel.*(acc, &1)
-  def do_eval(token, acc) when is_list(token), do: do_eval(evaluate(token), acc)
-
-  def test_input_2 do
-    """
-    1 + (2 * 3) + (4 * (5 + 6))
-    2 * 3 + (4 * 5)
-    5 + (8 * 3 + 9 + 3 * 4 * 3)
-    5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
-    ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
-    """
-  end
-
-  def solve_part_2(input) do
-    input
-    |> parse()
-    |> Enum.map(&evaluate_2/1)
-    |> Enum.sum()
-  end
-
-  def evaluate_2(token) when is_list(token) do
-    token
-    |> evaluate_2("+")
-    |> evaluate_2("*")
-  end
-
-  def evaluate_2(int) when is_integer(int), do: int
-
-  def evaluate_2(int, _operator) when is_integer(int), do: int
-  def evaluate_2([token], operator), do: evaluate_2(token, operator)
-  def evaluate_2([left | [operator | [right | rest]]], operator) do
-    op_func = to_op_func(operator)
-
-    evaluated =
-      left
-      |> evaluate_2()
-      |> op_func.(evaluate_2(right))
-
-    if Enum.empty?(rest) do
-      evaluated
+  def evaluate_reverse_polish([head | rest] = _rp_notation, stack) do
+    if is_integer(head) do
+      evaluate_reverse_polish(rest, [head | stack])
     else
-      evaluate_2([evaluated | rest], operator)
+      [l, r | stack_rest] = stack
+      evaluated = to_op_func(head).(l, r)
+      evaluate_reverse_polish([evaluated | rest], stack_rest)
     end
   end
 
-  def evaluate_2([left | [different_operator | rest]], operator) do
-    [left | [different_operator | [evaluate_2(rest, operator)]]]
+  def shunting_yard(expression, operator_precedences) do
+    expression
+    |> Enum.reduce({[], []}, fn token, {output_queue, operator_stack} ->
+      case token do
+        "(" ->
+          {output_queue, ["(" | operator_stack]}
+
+        ")" ->
+          match_paren({output_queue, operator_stack})
+
+        operator when operator in ["+", "*"] ->
+          handle_operator(operator, {output_queue, operator_stack}, operator_precedences)
+
+        int ->
+          {[String.to_integer(int) | output_queue], operator_stack}
+      end
+    end)
+    |> (fn {output_queue, operator_stack} -> Enum.reverse(output_queue) ++ operator_stack end).()
+  end
+
+  def match_paren({output_queue, ["(" | rest] = _operator_stack}), do: {output_queue, rest}
+
+  def match_paren({output_queue, [head | rest] = _operator_stack}),
+    do: match_paren({[head | output_queue], rest})
+
+  def handle_operator(
+        operator,
+        {output_queue, operator_stack},
+        operator_precedences
+      ) do
+    case Map.get(operator_precedences, Enum.at(operator_stack, 0)) do
+      nil ->
+        {output_queue, [operator | operator_stack]}
+
+      precedence ->
+        [head | rest] = operator_stack
+
+        if precedence >= Map.get(operator_precedences, operator) do
+          handle_operator(operator, {[head | output_queue], rest}, operator_precedences)
+        else
+          {output_queue, [operator | operator_stack]}
+        end
+    end
   end
 
   def to_op_func("+"), do: &Kernel.+/2
@@ -73,18 +81,9 @@ defmodule Day18 do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(fn line ->
-      body =
-        String.replace(line, ["(", ")", "+", "*", " "], fn
-          "(" -> "["
-          ")" -> "]"
-          "+" -> ~s/"+"/
-          "*" -> ~s/"*"/
-          " " -> ", "
-        end)
-
-      ("[" <> body <> "]")
-      |> Code.eval_string()
-      |> elem(0)
+      line
+      |> String.replace(" ", "")
+      |> String.graphemes()
     end)
   end
 end
